a2d5074ea0d65af9656782e2ccdc91f4343d980c
Sebastián Altmann <saaltmann@miuandes.cl>
2020-03-28 18:15:10 -0300

    Added 4 attributes to Cancion (nombre, album, artista, genero)

--- Laboratorio-02/Laboratorio-02/Cancion.cs
+++ Laboratorio-02/Laboratorio-02/Cancion.cs
@@ -3,6 +3,11 @@ namespace Laboratorio02
 {
     public class Cancion
     {
+        private string nombre;
+        private string album;
+        private string artista;
+        private string genero;
+
         public Cancion()
         {
         }
