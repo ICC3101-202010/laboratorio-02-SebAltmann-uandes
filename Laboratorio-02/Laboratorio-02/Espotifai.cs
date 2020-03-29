cfbc28335f309b11b2535a39a1afb6ad8ec14ad0
Sebastián Altmann <saaltmann@miuandes.cl>
2020-03-29 19:33:37 -0300

    Added method CancionesPorCriterio to class Espotifai

--- Laboratorio-02/Laboratorio-02/Espotifai.cs
+++ Laboratorio-02/Laboratorio-02/Espotifai.cs
@@ -43,5 +43,63 @@ namespace Laboratorio02
                 Console.WriteLine("No hay canciones en el gestor.");
             }
         }
+
+        public List<Cancion> CancionesPorCriterio(String criterio, String valor)
+        {
+            List<Cancion> CPC = new List<Cancion>();
+
+            if (criterio == "nombre")
+            {
+                foreach (Cancion Song in SongList)
+                {
+                    if (valor == Song.GetName())
+                    {
+                        CPC.Add(Song);
+                    }
+                }
+            } else if (criterio == "artista")
+            {
+                foreach (Cancion Song in SongList)
+                {
+                    if (valor == Song.GetArtist())
+                    {
+                        CPC.Add(Song);
+                    }
+                }
+            } else if (criterio == "album")
+            {
+                foreach (Cancion Song in SongList)
+                {
+                    if (valor == Song.GetAlbum())
+                    {
+                        CPC.Add(Song);
+                    }
+                }
+            } else if (criterio == "genero")
+            {
+                foreach (Cancion Song in SongList)
+                {
+                    if (valor == Song.GetGenre())
+                    {
+                        CPC.Add(Song);
+                    }
+                }
+            }
+
+            if (CPC.Count == 0)
+            {
+                Console.WriteLine("No existen canciones que cumplan los requisitos.");
+                Console.WriteLine(CPC);
+            } else if (CPC.Count == 1)
+            {
+                Console.WriteLine("La siguiente canción cumple con los requisitos de la búsqueda:");
+                Console.WriteLine(CPC);
+            } else
+            {
+                Console.WriteLine("Las siguientes canciones cumplen con los requisitos de la búsqueda:");
+                Console.WriteLine(CPC);
+            }
+            return CPC;
+        }
     }
 }
