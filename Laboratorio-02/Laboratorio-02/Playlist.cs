3e8222ecef937b83d22671a7574b12c812d00cec
Sebastián Altmann <saaltmann@miuandes.cl>
2020-03-30 18:36:52 -0300

    Added attributes and P_Informacion() method to Playlist class

--- Laboratorio-02/Laboratorio-02/Playlist.cs
+++ Laboratorio-02/Laboratorio-02/Playlist.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Laboratorio02
 {
     public class Playlist
     {
-        public Playlist()
+        private string name;
+        private List<Cancion> daList;
+
+        public Playlist(string p_name, List<Cancion> canciones)
         {
+            this.name = p_name;
+            this.daList = canciones;
+        }
+
+        public String P_Informacion()
+        {
+            String list_str = "";
+            foreach (Cancion song in daList)
+            {
+                list_str = list_str + song.Informacion() + "\n";
+            }
+            return "Nombre de la Playlist: "+name+ "\n"+list_str;
+        }
+
+        public String P_GetName()
+        {
+            return name;
         }
     }
 }
